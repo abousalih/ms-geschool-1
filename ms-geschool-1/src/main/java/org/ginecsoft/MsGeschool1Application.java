626e15e47a70377dc4b15fa75062bee8e6bcb2d9
ABOUSALIH <lahcenabousalih@gmail.com>
2018-10-07 00:41:58 +0100

    3eme commit

---
+++ ms-geschool-1/src/main/java/org/ginecsoft/MsGeschool1Application.java
@@ -0,0 +1,12 @@
+package org.ginecsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGeschool1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGeschool1Application.class, args);
+	}
+}
